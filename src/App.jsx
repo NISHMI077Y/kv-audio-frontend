f4450d0317e58390315e877c3e0999cda1fbb2fb
NISHMI077Y <samindyapremathunga77@gmail.com>
2025-03-28 13:49:40 +0530

    Add sidebar navigation with icons

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,36 @@
 
 import './App.css'
 import ProductCard from './components/productCard'
+import { MdOutlineAutoGraph } from "react-icons/md";
+import { MdOutlineDataSaverOn } from "react-icons/md";
+import { BsFillSpeakerFill } from "react-icons/bs";
+import { FaUserCircle } from "react-icons/fa";
+
+
+
+
+
 
 function App() {
   
   return (
-    <>
+    <div className= "w-full h-screen">
+      <div className="w-[400px] h-full bg-green-200">
+   <button className='w-full h-[40px] text[25px] font-bold flex justify-center items-center' > <MdOutlineAutoGraph />Dashboard</button>
+   <button className='w-full h-[40px] text[25px] font-bold flex justify-center items-center'> <MdOutlineDataSaverOn />Bookings</button>
+   <button className='w-full h-[40px] text[25px] font-bold flex justify-center items-center'> <BsFillSpeakerFill />Items</button>
+   <button className='w-full h-[40px] text[25px] font-bold flex justify-center items-center'> <FaUserCircle/> Users</button>
+
+
+
+
+      </div>
+<div className="w-full bg-red-900">
+
+</div>
       
-    </>
+    
+    </div>
   )
 }
 
